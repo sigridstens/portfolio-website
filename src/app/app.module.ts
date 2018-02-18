7f4cfdce1d64e61371177f79e5d455d6f989afc9
SigridStensvold <sigridstens@gmail.com>
2018-02-18 12:44:13 -0600

    header and footer now setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {RouterModule, Routes} from "@angular/router";
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {FooterComponent} from './footer/footer.component';
 
+
+const appRoutes: Routes = [
+  // {
+  //   path: 'home',
+  //   component: FeedComponent
+  // },
+  // {
+  //   path: 'about',
+  //   component: PostPageComponent
+  // },
+  // {
+  //   path: 'contact',
+  //   component: AboutComponent
+  // },
+  // {
+  //   path: 'portfolio',
+  //   component: CalendarComponent
+  // }
+  ];
+
+export const appRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    appRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
